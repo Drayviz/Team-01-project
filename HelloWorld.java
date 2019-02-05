5bac08de541313e4d854473f391c113e4ad93950
Drayviz <44074565+Drayviz@users.noreply.github.com>
2019-02-05 11:25:33 -0700

    Update HelloWorld.java
    hjkhkhk

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,7 @@ public class Tutorial1 {
 	public static void main(String[] args) {
 		//System.out.println("Hello world!");
 		Scanner input = new Scanner(System.in);
-		System.out.print("Please enter your name: ");
-		System.out.println("fsdfsdfsdf);
+		System.out.print("fghfghgfhg ");
 	}
 	
 }
