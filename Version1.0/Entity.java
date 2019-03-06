6ccae94216a47019d763ff00083cc0ab2463bfef
misaele <47364551+misaele@users.noreply.github.com>
2019-03-05 23:47:26 -0700

    Add files via upload

---
+++ Version1.0/Entity.java
@@ -0,0 +1,53 @@
+
+public class Entity {
+  private double atk;
+	private double def;
+	private double hp;
+	private int party;
+	private int state;
+	private int ap;
+
+	public Entity(double atk, double def, double hp, int party, int state, int ap) {
+		this.atk = new Double(atk);
+		this.def = new Double(def);
+		this.hp = new Double(hp);
+		this.party = new Integer(party);
+		this.state = new Integer(state);
+		this.ap = new Integer(ap);
+	}
+	//copy constructor
+	public Entity(Entity copy) {
+		this.atk = new Double(copy.atk);
+		this.def = new Double(copy.def);
+		this.hp = new Double(copy.hp);
+		this.party = new Integer(copy.party);
+		this.state = new Integer(copy.state);
+		this.ap = new Integer(copy.ap);
+	}
+	
+	//getters
+	public double getAtk() {
+		return new Double(atk);
+	}
+	public double getDef() {
+		return new Double(def);
+	}
+	public double getHp() {
+		return new Double(hp);
+	}
+	public int getParty() {
+		return new Integer(party);
+	}
+	public int getState() {
+		return new Integer(state);
+	}
+	public int getAp() {
+		return new Integer(ap);
+	}
+	
+	// setters
+	public void setHp(Entity target, double dmgDealt) {
+		target.hp = target.hp - (dmgDealt)*(target.def/10.0);
+	}
+	
+}
---
+++ Version1.0/Pieces.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Array;
+
+public class Pieces {
+    private ArrayList<Entity> humanPieces = new ArrayList<Entity>();
+    private ArrayList<Entity> aiPieces = new ArrayList<Entity>();
+
+    public ArrayList<Entity> getHumanPieces() {
+        ArrayList<Entity> decoy = new ArrayList<Entity>();
+        for (Entity h:humanPieces) {
+            decoy.add(new Entity(h));
+        }
+        return decoy;
+    }
+    
+    public ArrayList<Entity> getAIPieces() {
+        ArrayList<Entity> decoy = new ArrayList<Entity>();
+        for (Entity e:aiPieces) {
+            decoy.add(new Entity(e));
+        }
+        return decoy;
+    }   
+
+    public void addHumanPieces(){
+        int count = 0; //intended to individualize each piece; each piece will have a different token as their last parameter)
+        for (int a = 0; a < 3; a++) {
+            count = count + 1;
+            Entity hPiece = new Entity(10, 10, 50, 1, 1, 5, count);
+            humanPieces.add(new Entity(hPiece));
+        }
+    }
+
+    public void addAIPieces(){
+        int count = 0; //intended to individualize each piece; each piece will have a different token as their last parameter)
+        for (int b = 0; b < level.getNumOfEnemies(); b++) {
+            count = count + 1;
+            Entity ePiece = new Entity(10, 10, 50, 0, 1, 5, count);
+            aiPieces.add(new Entity(ePiece));
+        }
+    }   
+}
+
+/* Consider when a piece dies??? */
