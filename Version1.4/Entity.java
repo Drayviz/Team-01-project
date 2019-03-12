1f23f04af8785dd3215f200dfe8d920096bf31a0
DanielChau43 <47364498+DanielChau43@users.noreply.github.com>
2019-03-11 20:30:17 -0600

    Update Entity.java
    add documentation

--- Version1.4/Entity.java
+++ Version1.4/Entity.java
@@ -2,7 +2,7 @@
 import java.util.Random;
 
 public class Entity extends MetaGame{
-	
+	/** Instance variables for the class that are set to default values */
 	Random r = new Random();
 
 	private int atk;
@@ -20,12 +20,24 @@ public class Entity extends MetaGame{
 	private Weapon weapon = new Weapon();
 	private int uniqueid = 0;
 	private int prefabidentifyer = 0;
-
+	
+	/** Constructor */
 	Entity()
 	{
 
 	}
-
+	
+	/** Constructor.
+     	* @param name Takes name as a parameter. 
+     	* @param atk Takes atk as a parameter.
+     	* @param def Takes def as a parameter.
+	* @param hp Takes hp as a parameter. 
+     	* @param party Takes party as a parameter.
+     	* @param state Takes state as a parameter
+	* @param ap Takes ap as a parameter.
+     	* @param movement Takes movement as a parameter.
+	* @param prefabidentifyer Takes prefabidentifyer as a parameter. 
+	*/
 	public Entity(String name, int atk, int def, int hp, int party, int state, int ap, int movement,int prefabidentifyer) {
 		this.atk = new Integer(atk);
 		this.def = new Integer(def);
@@ -41,7 +53,8 @@ public class Entity extends MetaGame{
 		this.prefabidentifyer = prefabidentifyer;
 		//weapon = new Weapon(prefabidentifyer);
 	}
-
+	/** Copy constructor
+	* @param copy Takes an Entity object as an argument and creates a copy of it*/
 	public Entity(Entity copy) {
 		this.atk = copy.getAtk();
 		this.def = copy.getDef();
@@ -60,61 +73,105 @@ public class Entity extends MetaGame{
 		this.prefabidentifyer = copy.getPrefabIdentifyer();
 	}
 
-	
+	/** Getter. 
+     	* @return prefabidentifyer. Used to return the instance variable prefabidentifyer.*/
 	public int getPrefabIdentifyer()
 	{
 		return new Integer(prefabidentifyer);
 	}
-
+	
+	/** Getter. 
+     	* @return kills. Used to return the instance variable kills.*/
 	public int getKills()
 	{
 		return new Integer(kills);
 	}
+	
+	/** Getter. 
+     	* @return xp. Used to return the instance variable xp.*/
 	public int getXp() {
 		return new Integer(xp);
 	}
+	
+	/** Getter. 
+     	* @return maxhp. Used to return the instance variable maxhp.*/
 	public int getMaxhp()
 	{
 		return new Integer(maxhp);
 	}
+	
+	/** Getter. 
+     	* @return name. Used to return the instance variable name.*/
 	public String getName()
 	{
 		return name;
 	}
+	
+	/** Getter. 
+     	* @return uniqueid. Used to return the instance variable uniqueid.*/
 	public int getUniqueId() {
 		return new Integer(uniqueid);
 	}
+	
+	/** Getter. 
+     	* @return weapon. Used to return the instance variable weapon.*/
 	public Weapon getWeapon(){
 		return new Weapon(weapon);
 	}
+	
+	/** Getter. 
+     	* @return atk. Used to return the instance variable atk.*/
 	public int getAtk() {
 		return new Integer(atk);
 	}
 	
+	/** Getter. 
+     	* @return def. Used to return the instance variable def.*/
 	public int getDef() {
 		return new Integer(def);
 	}
+	
+	/** Getter. 
+     	* @return hp. Used to return the instance variable hp.*/
 	public int getHp() {
 		return new Integer(hp);
 	}
+	
+	/** Getter. 
+     	* @return party. Used to return the instance variable party.*/
 	public int getParty() {
 		return new Integer(party);
 	}
+	
+	/** Getter. 
+     	* @return state. Used to return the instance variable state.*/
 	public int getState() {
 		return new Integer(state);
 	}
+	
+	/** Getter. 
+     	* @return ap. Used to return the instance variable ap.*/
 	public int getAP() {
 		return new Integer(ap);
 	}
+	
+	/** Getter. 
+     	* @return movement. Used to return the instance variable movement.*/
 	public int getMovement()
 	{
 		return new Integer(movement);
 	}
+	
+	/** Getter. 
+     	* @return maxap. Used to return the instance variable maxap.*/
 	public int getMaxap() 
 	{
 		return new Integer(maxap);
 	}
-
+	
+	/**
+	*Function used to check state of pieces 
+	*@returns test. Used to return the state of the piece*/
 	public int checkstate()
 	{
 		int test = 0;
@@ -125,11 +182,17 @@ public class Entity extends MetaGame{
 		}
 		return test;
 	}
+	
+	/**
+	*Keeps track of action points which allows pieces to attack */
 	public void ActionTakes(int points)
 	{
 		this.ap = this.ap - points;
 	}
 	
+	/**Setter.
+    	* Used to set the value for the instance variable <hp>.
+    	* @param target This is new target they'd like to set.*/
 	public void setHp(Entity target)
 	{
 		target.hp = target.hp - (atk - target.def);
@@ -139,34 +202,47 @@ public class Entity extends MetaGame{
 			this.addKill();
 		}
 	}
-
+	/**
+	*@param ammount Takes ammount as a parameter 
+	*Adds xp gained to the variable xp */
 	public void addXp(int ammount)
 	{
 		xp += ammount;
 	}
-
+	
+	/**
+	*Every kill increments kills by one, used to keep track of total kills */
 	public void addKill()
 	{
 		kills ++;
 	}
 
-
+	/**Setter.
+     	* Used to set the value for the instance variable <name>.
+     	* @param name This is new name they'd like to set.*/
 	public void setName(String name)
 	{
 		this.name = name;
 	}
-
+	
+	/**
+	*Each heal increments by one
+	*/
 	public void heal()
 	{
 		this.hp ++;
 	}
-
+	
+	/**
+	*Function used to restore maxphp and maxap */
 	public void rejuvinate()
 	{
 		this.hp = this.maxhp;
 		this.ap = this.maxap;
 	}
-
+	
+	/**
+	*Function used to reset maxap back to its original */
 	public void resetap()
 	{
 		ap = getMaxap();
