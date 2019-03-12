b01a077cf24016764d5068e9a83cbbb98f8602be
Drayviz <44074565+Drayviz@users.noreply.github.com>
2019-03-12 02:26:30 -0600

    Add files via upload
    This is the GUI version for Demo 2.

---
+++ Version1.4.GUI/BoardPanel.java
@@ -0,0 +1,201 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import java.util.ArrayList;
+
+public class BoardPanel extends Application {
+
+    /** Instance variables. The first four are to initialize various objects that the game needs to run,
+     * while the bottom four are variables consistently used and changed throughout the usage of the GUI. */
+    private Map map;
+    private MetaGame startGame = new MetaGame();
+    private Game game = new Game();
+    private Pieces pieceLists = new Pieces();
+
+    private Label toPlayer = new Label("Welcome!");
+    private String submessage = "---------";
+    private String message;
+    private int piecesPlaced = 0;
+
+    /** This method sets up the appearance of the GUI itself, while also eventhandling when boardButton,
+     * viewParty, viewEnemies, and endTurn buttons are clicked.
+     *
+     * Note: this method is incomplete in terms of running the game. Currently, the GUI can place AI pieces and
+     * allow the player to place pieces, as well as allowing the player to make "moves". However, the player cannot
+     * move nor attack pieces. Furthermore, the GUI stops responding after a seemingly arbitrary number of loops.
+     * These aforementioned loops are run by the endTurn button, which calls the play() method in Game.
+     *
+     * Note 2: the GUI version of the game differs fromt the console-based. There are different methods, so far, in
+     * MetaGame and Game however all other classes are similar.*/
+	@Override
+    public void start(Stage primaryStage) {
+        /*These are the primary layouts used in the GUI.*/
+		BorderPane root = new BorderPane();
+        VBox topSection = new VBox(0);
+        HBox underTop = new HBox();
+        GridPane grid = new GridPane();
+
+        /*Setting up the very important toPlayer label, which is set to change the message when most
+        buttons are pressed.*/
+        toPlayer.setFont(Font.font("Courier New", 18));
+        //toPlayer.setTextFill(Color.WHITE);
+        toPlayer.setAlignment(Pos.CENTER);
+
+        /*Creation of most of the buttons. So far, "Move" and "Attack Order" are dead buttons.*/
+        Button endTurn = new Button("End Turn");
+        Button move = new Button("Move");
+        Button viewParty = new Button("View party");
+        Button viewEnemies = new Button("View enemies");
+        Button attackOrder = new Button("Attack Order");
+
+        HBox rightButton = new HBox(attackOrder);
+        rightButton.setAlignment(Pos.CENTER_RIGHT);
+        HBox.setHgrow(rightButton, Priority.ALWAYS);
+
+        underTop.getChildren().addAll(endTurn, move, viewParty, viewEnemies, rightButton);
+        underTop.setPadding(new Insets(2));
+
+        topSection.getChildren().addAll(toPlayer, underTop);
+
+        grid.setVgap(20);
+        grid.setHgap(20);
+        grid.setAlignment(Pos.CENTER);
+
+        //C O M M U N I C A T I O N
+        /*This runs the method gamePlaying() in this class. (See below)*/
+        gamePlaying();
+
+        /*This is the initial map creation. The loop is supposed to create buttons in a grid (using GridPane),
+         with the rows and columns equal to the map dimensions.*/
+        for(int row = 0; row < map.getDimensions(); row++) {
+            for(int col = 0; col < map.getDimensions(); col++) {
+                grid.setStyle("-fx-background-color: GREEN;");
+                int place = conversion(row,col,map.getDimensions()); //This runs the method conversion() in this class. (See below)
+                findAIPieces(place-1); //This runs the method findAIPieces() in this class. (See below)
+                message = place + ", " + submessage;
+                Button boardButton = new Button(message);
+                grid.add(boardButton, col, row);
+                boardButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        int place = conversion(GridPane.getRowIndex(boardButton),GridPane.getColumnIndex(boardButton),map.getDimensions()); //This runs the method conversion() in this class. (See below)
+                        if (game.getTurnCounter() == 0 && piecesPlaced < pieceLists.getPlayerParty().size()) { //This ensures that pieces can only be placed before the game begins, and there can't be more pieces placed than permitted.
+                            if (map.getPiece(place) == 0 && place < (map.getDimensions() * map.getDimensions() - map.getDimensions() * 3)) { //This ensures that pieces can only be placed on empty spaces and not in the last 3 rows
+                                map.setState(place, 1, piecesPlaced); //This sets the piece itself
+                                submessage = "PIECE " + (piecesPlaced+1);
+                                message = place + ", " + submessage;
+                                boardButton.setText(message);
+                                piecesPlaced++;
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+        topSection.setPadding(new Insets(10, 30, 30, 30));
+        topSection.setSpacing(5);
+
+        root.setPadding(new Insets(30,30,30,30));
+        root.setCenter(grid);     
+		root.setTop(topSection);
+        //root.setStyle("-fx-background-color: BLACK;");
+
+		Scene scene = new Scene(root, 640, 480);
+	    primaryStage.setTitle("Planet Invaders");
+	    primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+        //E V E N T H A N D L I N G
+        /* This is for the viewParty button */
+        viewParty.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String forDisplay = "Party: ";
+                for(Entity e:pieceLists.getPlayerParty()) {
+                    forDisplay = forDisplay + e.getName() + " (atk: " + e.getAtk() + ",hp: " + e.getHp() + ",mvt: " + e.getMovement() + ") ";
+                }
+                toPlayer.setText(forDisplay);
+            }
+        });
+        /*This is for the viewEnemies button*/
+        viewEnemies.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String forDisplay = "Party: ";
+                for(Entity e:pieceLists.getAIPieces()) {
+                    forDisplay = forDisplay + e.getName() + " (atk: " + e.getAtk() + ",hp: " + e.getHp() + ",mvt: " + e.getMovement() + ") ";
+                    //forDisplay = forDisplay + "1";
+                }
+                toPlayer.setText(forDisplay);
+            }
+        });
+        /*This is for the endTurn button.
+        The last two lines are special:
+        The first of the two sets an instance variable <loopRun> in Game to true, which is what allows play() to run.
+        The second line actually runs play().*/
+        endTurn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                game.setTurnCounter();
+                //System.out.println("Turn " + game.getTurnCounter());
+                game.setLoopRun(true);
+                game.play();
+
+            }
+        });
+    }
+
+    /**This method is used to instantiate the objects necessary to start the game so that the game can be run through the same class as the GUI
+    * (this serves to start the GUI and the game itself simultaneously)
+    * All these commands are meant to set the game up, before the turn loop runs.*/
+    public void gamePlaying() {
+        this.game = startGame.startgame("five");
+        this.pieceLists = game.getPieces();
+        this.map = game.getMap();
+        game.placeAIPieces();
+        if (game.getTurnCounter() == 0) {
+            toPlayer.setText("Place your pieces! ");
+        }
+    }
+
+    /**This method is necessary because the row and column numbers of the grid don't correspond to anything.
+    * Thus, this method converts the row and column numbers into one integer that increases for each button added.
+    *@param rowIndex . This is the row the button is in.
+     *@param columnIndex . This is the column the button is in.
+     *@param dimensions . This is the dimensions of the map itself.*/
+    public int conversion(int rowIndex, int columnIndex, int dimensions) {
+        return dimensions - (dimensions - columnIndex - 1) + dimensions*rowIndex;
+    }
+
+    /**This goes hand-in-hand with placeAIPieces() in Game. This ensures that the enemy pieces are already placed when the map is created.
+    *@param index . This is the converted number (using conversion()) of the location of the button.*/
+    public void findAIPieces(int index) {
+        ArrayList<ArrayList<Integer>> proxy = map.getMaparray();
+	    ArrayList<Integer> tilearray = proxy.get(index);
+	    if (tilearray.get(1) != 0) {
+	        submessage = "ENEMY " + tilearray.get(1);
+        }
+	    else {
+            submessage = "---------";
+        }
+    }
+
+	public static void main(String[] args) {
+        launch(args);	
+	}
+}
