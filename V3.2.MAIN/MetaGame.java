7eefd384f564ccdb655ca5258d60084ce8352343
Lana Nieves <lananieves55@gmail.com>
2019-04-08 13:36:54 -0600

    I FIXED METAGAME

--- V3.2.MAIN/MetaGame.java
+++ V3.2.MAIN/MetaGame.java
@@ -322,6 +322,6 @@ public class MetaGame
         String[] Value = fileinput.split(" ");
         fileinput = Value[0];
         String fileinput2 = Value[1];
-        a.startGUIGame(fileinput,fileinput2);
+        a.startgame(fileinput,fileinput2);
     }
 }
